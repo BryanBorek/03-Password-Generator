f0ee6022633976aa423a1b1bf2564cf697599d84
Bryan Borek <bryantborek@gmail.com>
2022-07-21 16:36:35 -0500

    completed readme and made minor tweaks to alerts

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,14 @@
 // Assignment Code
 
+//arrays containing characters possible
 var uppercaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowercaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var specialCharacters = ['~','`','!','@','#','$','%','^','&','*','(',')','-','_','+','=','{','}','[',']','|',',',':',';','"','<','>','.',',','?'];
 var numericCharacters = ['0','1','2','3','4','5','6','7','8','9'];
-var password = "";
-var selectedCharacters = [];
-var passwordLength = 0;
+
+var password = ""; //password string
+var selectedCharacters = []; //array containing concacted arrays chosen
+var passwordLength = 0; 
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -14,10 +16,10 @@ function writePassword() {
   password = ""; //resets password
   selectedCharacters = []; //resets selected characters
   password = generatePassword(); //calls the generatePassword function to begin
+  
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
 }
 
 //Generate password to be written
@@ -26,34 +28,35 @@ function generatePassword() {
   if(passwordLength > 8 && passwordLength < 128) {
     chooseCharacters();
   }
-  if(selectedCharacters.length > 0) {
+  if(selectedCharacters.length > 0) { //stop from displaying on window if password length undefined
     for(var i=0; i<passwordLength; i++) {
-  
       password = password + selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)];
   }
-
 }
 return password;
 }
 
 function chooseLength() {
   
-  passwordLength = prompt("How long do you want your password to be? Choose between 8 and 128.")
+  passwordLength = prompt("How long would you like your password to be? Choose between 8 and 128.")
   if(passwordLength === null) {
-    alert("I guess you changed your mind.");
+    alert("Oh, I guess you changed your mind.");
     return;
   }
   if(passwordLength === "") {
-    alert("Please type a number before you click OK");
+    alert("Please enter a number before you click OK");
+    return;
+  }
+  if(passwordLength === "0") {
+    alert("How are we supposed to make a password 0 characters long? Do better.");
     return;
   }
   if(isNaN(passwordLength)) {
-    alert("Thats not a number!");
+    alert("Did your cat get ahold of your keyboard? Numbers only please.");
     return;
   }
   else if(passwordLength < 8) {
     alert("A brute force cracker could determine that password in less than 4 days. Try typing a number between 8 and 128 next time.");
-    console.log(passwordLength);
     return;
   }
   else if(passwordLength > 128) {
@@ -61,11 +64,12 @@ function chooseLength() {
     return;
   }
   else {
-    alert("You chose " + passwordLength + " characters long!");
+    alert("Your password will be " + passwordLength + " beautiful characters long.");
   }
-
 }
 
+
+// confirm booleans for whether or not the user wants to include uppercase, lowercase, special, and numeric characters.
 function chooseCharacters() {
   var isUppercase = confirm("Would you like to use Uppercase Characters?");
   var isLowercase = confirm("Would you like to use Lowercase Characters?");
@@ -75,32 +79,27 @@ function chooseCharacters() {
  if(isUppercase) {
     selectedCharacters = selectedCharacters.concat(uppercaseCharacters);
     alert("Your password will include Uppercase Characters");
-    console.log(isUppercase);
   }
   
   if(isLowercase) {
     selectedCharacters = selectedCharacters.concat(lowercaseCharacters);
     alert("Your password will include Lowercase Characters");
-    console.log(isLowercase);
   }
   
   if(isSpecial) {
     selectedCharacters = selectedCharacters.concat(specialCharacters);
     alert("Your password will include Special Characters");
-    console.log(isSpecial);
   }
   
   if(isNumeric) {
     selectedCharacters = selectedCharacters.concat(numericCharacters);
     alert("Your password will include Numeric Characters");
-    console.log(isNumeric);
   }
 
-  if(isUppercase + isLowercase + isSpecial + isNumeric === 0) {
-    alert("I guess you changed your mind.");
+  if(isUppercase + isLowercase + isSpecial + isNumeric === 0) {  //if no characters chosen do not continue
+    alert("Oh, I guess you changed your mind.");
     return;
   }
-  console.log(selectedCharacters);
 }
 
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # 03 JavaScript: Password Generator
 
+Link to deployed app: https://bryanborek.github.io/03-Password-Generator/ 
+
+Link to the project Github: https://github.com/BryanBorek/03-Password-Generator
+
+![Bryan's Password Generator](Assets/passGenScreenshot.png)
+
+This password generator allows the user to generate a password between 8-128 characters in length.
+
+The user can choose to add up to 4 different types of characters to be randomly selected. These types are: Uppercase, Lowercase, Special, and Numeric.
+
+Validation is given for the length and options selected. 
+
+Password is then written to the page
